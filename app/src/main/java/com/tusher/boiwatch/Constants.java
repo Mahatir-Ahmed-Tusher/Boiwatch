3d49ac72d0d0c8ba7c2767fef1385e86b3fc956e
Mahatir-Ahmed-Tusher <mahatirtusher@gmail.com>
2026-03-08 16:00:35 +0600

    groq API key is hidden in constrant.java

--- app/src/main/java/com/tusher/boiwatch/Constants.java
+++ app/src/main/java/com/tusher/boiwatch/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String TMDB_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzIwMmZiOWI4OTg5ZDkwNTI1MDNmYzQxMzk3MzkzMiIsIm5iZiI6MTc1NTc1NDIxNS4zNzUsInN1YiI6IjY4YTZhZWU3ZjNlM2VmZmNiOTY5Nzc0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5zVAYxPV8_P7xS2wrMSXohyJsv9bqbKFNvBfo4d0FX4";
     
     public static final String GROQ_BASE_URL = "https://api.groq.com/openai/v1/";
-    public static final String GROQ_API_KEY = "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";
+    public static final String GROQ_API_KEY = "PUT_YOUR_API_KEY_HERE";
     
     public static final String PREF_NAME = "BoiWatchPrefs";
     public static final String KEY_WATCH_HISTORY = "watch_history";
